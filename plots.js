b92c2dc5b52a91f5c8a9dc57a0ffbe3451f99bb3
Kelvin Mutua Kilonzo <kelvinmutua959@gmail.com>
2022-07-21 13:16:11 -0500

    add files

--- plots.js
+++ plots.js
@@ -1,37 +1,133 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+//Generate Dropdown Menu Items: ID numbers.
+function init() {
+  var selector = d3.select('#selDataset');
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+  d3.json('samples.json').then((data) => {
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector.append('option').text(sample).property('value', sample);
+    });
+    var initialSample = sampleNames[0];
+    buildMetadata(initialSample);
+    buildCharts(initialSample);
+  });
+}
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
+init();
 
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
+// optionChanged is called when a change takes place in this tag in the HTML. 
+// this.value = newSample = ID number
 
-// data
-var data = [trace1];
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
 
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
-};
+// Print information to the "Demographic Info" once a user selects an ID number.
+function buildMetadata(sample) {
+  d3.json('samples.json').then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter((sampleObj) => sampleObj.id == sample);
+    var pairs = Object.entries(resultArray[0]);
+    var PANEL = d3.select('#sample-metadata');
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+    PANEL.html('');
+    var results = pairs.forEach(function (pair) {
+      PANEL.append('h6').text(pair[0] + ': ' + pair[1]);
+    });
+  });
+}
+
+//Build the charts once a user selects an ID number.
+function buildCharts(sample) {
+  d3.json('samples.json').then(function ({ samples, metadata }) {
+    var data = samples.filter((obj) => obj.id == sample)[0];
+    console.log(data);
+
+    // Bar chart data
+    var otuIDS = data.otu_ids.map((row) => `OTU ID: ${row}`);
+    var sampleValues = data.sample_values.slice(0, 10);
+    var sampleLabels = data.otu_labels.map((label) =>
+      label.replace(/\;/g, ', ')
+    );
+
+    // Bubble chart data
+    var otuID = data.otu_ids;
+    var sampleValue = data.sample_values;
+    var sampleLabel = data.otu_labels.map((label) =>
+      label.replace(/\;/g, ', ')
+    );
+
+    // Guage data
+    var metData = metadata.filter((obj) => obj.id == sample)[0];
+    var washFreq = metData.wfreq;
+
+    // Bar chart data
+    var data1 = [
+      {
+        x: sampleValues,
+        y: otuIDS,
+        type: 'bar',
+        orientation: 'h',
+        text: sampleLabels,
+        hoverinfo: 'text',
+      },
+    ];
+
+    // Bubble chart data
+    var data2 = [
+      {
+        x: otuID,
+        y: sampleValue,
+        mode: 'markers',
+        text: sampleLabel,
+        marker: {
+          size: sampleValue,
+          color: otuID,
+        },
+      },
+    ];
+    // Guage chart data
+    var data3 = [
+      {
+        // domain: washFreq,
+        value: washFreq,
+        title: {
+          text: 'Belly Button Washing Frequency<br>Scrubs per Week',
+        },
+        type: 'indicator',
+        mode: 'gauge+number',
+        gauge: {
+          axis: { range: [null, 10] },
+        },
+      },
+    ];
+
+    // Bar chart layout
+    var layout1 = {
+      margin: {
+        t: 40,
+        l: 150,
+      },
+      title: {
+        text: 'Top 10 Bacterial Species (OTUs)',
+      },
+    };
+
+    // Bubble chart layout
+    var layout2 = {
+      xaxis: { title: 'OTU ID' },
+    };
+
+    // Gauge chart layout
+    var layout3 = {
+      width: 600,
+      height: 500,
+      margin: { t: 0, b: 0 },
+    };
+
+    Plotly.newPlot('bar', data1, layout1);
+    Plotly.newPlot('bubble', data2, layout2);
+    Plotly.newPlot('gauge', data3, layout3);
+  });
+}
